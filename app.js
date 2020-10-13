106cf848c62853094fbac8f510269c0a559f966b
Wakidur Rahman <wakidur@gmail.com>
2020-10-13 13:27:07 +0600

    DB connection and event model create

--- app.js
+++ app.js
@@ -3,28 +3,53 @@ const bodyParser = require('body-parser');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
 const dotenv = require('dotenv').config({ path: '.env' });
+const connectDB = require('./config/db-config');
 
-/**
- * Load environment variables from .env file, where API keys and passwords are configured.
- */
-
+// Instantiate Express app
 const app = express();
+// Connect to database
+connectDB();
 app.use(bodyParser.json());
 
 app.get('/', (req, res, next) => {
   res.send('Hello World');
 });
 
+const events = [];
+
 // GraphQL Middleware
 app.use(
   '/graphql',
   graphqlHTTP({
     schema: buildSchema(`
+    type Event {
+        _id: ID!,
+        name: String!,
+        brandName: String!,
+        details: String!,
+        image: String!,
+        price: Float!,
+        category: String!,
+        createdAt: String!,
+        updateAt: String!
+    }
+
+    input EventInput {
+        name: String!,
+        brandName: String!,
+        details: String!,
+        image: String!,
+        price: Float!,
+        category: String!,
+        createdAt: String!,
+        updateAt: String!
+    }
+
     type RootQuery {
-        events: [String!]!
+        events: [Event!]!
     }
     type RootMutation{
-        createEvent(name: String): String
+        createEvent(eventInput: EventInput): Event
     }
     schema {
         query: RootQuery
@@ -33,11 +58,22 @@ app.use(
     `),
     rootValue: {
       events: () => {
-        return ['Romantic Cooking', 'Sailing', 'All-Night Coding'];
+        return events;
       },
       createEvent: (args) => {
-        const eventName = args.name;
-        return eventName;
+        const event = {
+          _id: Math.random().toString(),
+          name: args.eventInput.name,
+          brandName: args.eventInput.brandName,
+          details: args.eventInput.details,
+          image: args.eventInput.image,
+          price: +args.eventInput.price,
+          category: args.eventInput.category,
+          createdAt: args.eventInput.createdAt,
+          updateAt: args.eventInput.updateAt,
+        };
+        events.push(event);
+        return event;
       },
     },
     graphiql: true,
